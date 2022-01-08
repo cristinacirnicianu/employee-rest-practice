a354d0b324eb23e9a7fdb91f8f4952abec7e5923
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2022-01-08 12:14:33 +0100

    Create an entity class

---
+++ src/main/java/com/practice/employee/model/Employee.java
@@ -0,0 +1,22 @@
+package com.practice.employee.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="emp_id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_id")
+    private String emailId;
+}
